50503de024e857aab4f39179f8e05325b4541043
Jason <pix3l.icious@gmail.com>
2022-04-27 19:33:15 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
