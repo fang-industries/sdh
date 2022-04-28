de8e6b84a62be01fff327d62afe9cff026abd2d1
Jason <pix3l.icious@gmail.com>
2022-04-28 23:07:06 +0700

    formatting

--- src/components/pages/home/Statistics.jsx
+++ src/components/pages/home/Statistics.jsx
@@ -7,12 +7,18 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function () {
-  return (
-    <div className="grid h-80 grid-cols-4 gap-8 bg-sky-900 px-16 py-8">
-      <Card icon={faPeopleGroup} number="12,530,672" stat="Active Students" />
-      <Card icon={faCertificate} number="8" stat="Certified Teachers" />
-      <Card icon={faTrophy} number="-3" stat="International Awards" />
-      <Card icon={faGraduationCap} number="11" stat="Graduates" />
-    </div>
-  );
+    const students = 12606275;
+
+    return (
+      <div className="grid h-80 grid-cols-4 gap-8 bg-sky-900 px-16 py-8">
+        <Card
+          icon={faPeopleGroup}
+          number={students.toLocaleString()}
+          stat="Active Students"
+        />
+        <Card icon={faCertificate} number="8" stat="Certified Teachers" />
+        <Card icon={faTrophy} number="-3" stat="International Awards" />
+        <Card icon={faGraduationCap} number="11" stat="Graduates" />
+      </div>
+    );
 }
