d043f9f967d934edb24f92f182aad85e2c7ab77c
Jason <pix3l.icious@gmail.com>
2022-04-30 07:26:35 +0700

    make smaller fix opacity

--- src/components/pages/home/Join.jsx
+++ src/components/pages/home/Join.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 export default function () {
   return (
-    <div className="mx-16 mb-16 flex justify-between rounded-xl bg-sky-900 p-8 text-3xl font-semibold text-neutral-50">
-      <div className="my-auto">
-        <p className="">Be a part of our lifelong,</p>
-        <p className="">abusive learning community today.</p>
+    <div className="mx-16 mb-16 flex justify-between rounded-xl bg-sky-900 p-8 font-semibold text-neutral-50">
+      <div className="my-auto text-2xl">
+        <p>Be a part of our lifelong,</p>
+        <p>abusive learning community today.</p>
       </div>
       <div className="flex flex-col space-y-4">
         <Link
           to="/registration"
-          className="flex transform rounded-full border-2 border-white bg-neutral-300 bg-opacity-50 px-3 py-2 text-lg duration-300 ease-in-out hover:-translate-y-0.5 hover:border-yellow-500 hover:bg-yellow-500 hover:text-neutral-50 dark:bg-neutral-500"
+          className="flex transform rounded-full border-2 border-white bg-neutral-300 bg-opacity-30 px-3 py-2 duration-300 ease-in-out hover:-translate-y-0.5 hover:border-yellow-500 hover:bg-yellow-500 hover:text-neutral-50 dark:bg-neutral-500 dark:bg-opacity-30"
         >
           {/* this button doesn't work yet! */}
           <FontAwesomeIcon icon={faGlobe} className="my-auto mr-2" />
@@ -20,7 +20,7 @@ export default function () {
         </Link>
         <Link
           to="/admissions"
-          className="flex transform rounded-full border-2 border-white bg-neutral-300 bg-opacity-50 px-3 py-2 text-lg duration-300 ease-in-out hover:-translate-y-0.5 hover:border-yellow-500 hover:bg-yellow-500 hover:text-neutral-50 dark:bg-neutral-500"
+          className="flex transform rounded-full border-2 border-white bg-neutral-300 bg-opacity-50 px-3 py-2 duration-300 ease-in-out hover:-translate-y-0.5 hover:border-yellow-500 hover:bg-yellow-500 hover:text-neutral-50 dark:bg-neutral-500 dark:bg-opacity-30"
         >
           {/* this button doesn't work yet! */}
           <FontAwesomeIcon icon={faSchool} className="my-auto mr-2" />
