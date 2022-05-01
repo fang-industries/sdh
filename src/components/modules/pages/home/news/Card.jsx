0e72d9c5e3532360b726421b44d3baf2f29c98fa
Jason <pix3l.icious@gmail.com>
2022-05-01 22:49:55 +0700

    accidentally made these class instead of className

--- src/components/modules/pages/home/news/Card.jsx
+++ src/components/modules/pages/home/news/Card.jsx
@@ -9,16 +9,16 @@ import { Link } from "react-router-dom";
 
 export default function (props) {
   return (
-    <div class="rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
+    <div className="rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
       <img
-        class="mx-auto rounded-xl object-cover"
+        className="mx-auto rounded-xl object-cover"
         src={props.img}
         alt={`Cover art for ${props.topic.toLowerCase()}`}
       />
-      <div class="mt-8 flex flex-col justify-between space-y-2 text-left">
-        <p class="text-yellow-500">{props.topic}</p>
-        <p class="text-lg font-medium">{props.title}</p>
-        <p class="text-sky-600 dark:text-neutral-300">{props.desc}</p>
+      <div className="mt-8 flex flex-col justify-between space-y-2 text-left">
+        <p className="text-yellow-500">{props.topic}</p>
+        <p className="text-lg font-medium">{props.title}</p>
+        <p className="text-sky-600 dark:text-neutral-300">{props.desc}</p>
         <Link
           to={props.news}
           className="dark:text-neurtal-500 mt-8 flex space-x-2 text-neutral-600 hover:underline hover:decoration-sky-500 dark:text-neutral-400"
@@ -28,7 +28,7 @@ export default function (props) {
         </Link>
       </div>
       <div className="my-8 border-2 border-b border-neutral-200 dark:border-neutral-700"></div>
-      <div class="flex space-x-4 text-left">
+      <div className="flex space-x-4 text-left">
         <p>
           <FontAwesomeIcon icon={faEye} /> {props.stats.views}
         </p>
--- src/components/pages/home/Alumni.jsx
+++ src/components/pages/home/Alumni.jsx
@@ -29,23 +29,23 @@ export default function () {
         pagination={{ clickable: true }}
       >
         <SwiperSlide>
-          <div class="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
+          <div className="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
             <img
-              class="mx-auto my-auto h-32 w-32 rounded-full object-cover"
+              className="mx-auto my-auto h-32 w-32 rounded-full object-cover"
               src="https://raw.githubusercontent.com/sekolah-daging-habituasi/assets/main/alumni/timothy.png"
               alt="Picture of Timothy Joseph"
             />
-            <div class="ml-8 flex flex-col justify-between space-y-8 text-left">
-              <p class="text-lg font-medium">
+            <div className="ml-8 flex flex-col justify-between space-y-8 text-left">
+              <p className="text-lg font-medium">
                 SDH shows discipline and highly value Ayam Goreng Padang. Since
                 the start, SDH has consistently has had the option to show great
                 labour between all educators, staff, understudies and guardians.
               </p>
-              <div class="font-medium">
-                <div class="text-yellow-500">
+              <div className="font-medium">
+                <div className="text-yellow-500">
                   Timothy Joseph, Dog food taster
                 </div>
-                <div class="text-neutral-7000">
+                <div className="text-neutral-7000">
                   SDH Daan Mogot, Class of 1789
                 </div>
               </div>
@@ -53,24 +53,24 @@ export default function () {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
+          <div className="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
             <img
-              class="mx-auto my-auto h-32 w-32 rounded-full object-cover"
+              className="mx-auto my-auto h-32 w-32 rounded-full object-cover"
               src="https://raw.githubusercontent.com/sekolah-daging-habituasi/assets/main/alumni/alexa.png"
               alt="Picture of Alexa Tan"
             />
-            <div class="ml-8 flex flex-col justify-between space-y-8 text-left">
-              <p class="text-lg font-medium">
+            <div className="ml-8 flex flex-col justify-between space-y-8 text-left">
+              <p className="text-lg font-medium">
                 I studied at SDH Daan Mogot since I was a toddler until I was
                 349 years old, same as my siblings. I&apos;ve experienced a lot
                 of great things at this school. And here I am, finding myself as
                 a professional sleeper, all because of the education in SDH.
               </p>
-              <div class="font-medium">
-                <div class="text-yellow-500">
+              <div className="font-medium">
+                <div className="text-yellow-500">
                   Alexa Tan, Professional sleeper
                 </div>
-                <div class="text-neutral-7000">
+                <div className="text-neutral-7000">
                   SDH Daan Mogot, Class of 1862
                 </div>
               </div>
@@ -79,22 +79,24 @@ export default function () {
         </SwiperSlide>
         <SwiperSlide>
           {/* TODO: Change person */}
-          <div class="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
+          <div className="flex rounded-xl bg-neutral-100 p-8 dark:bg-neutral-800">
             <img
-              class="mx-auto my-auto h-32 w-32 rounded-full object-cover"
+              className="mx-auto my-auto h-32 w-32 rounded-full object-cover"
               src="https://raw.githubusercontent.com/sekolah-daging-habituasi/assets/main/alumni/jovan.png"
               alt="Picture of Jovannes Adryan"
             />
-            <div class="ml-8 flex flex-col justify-between space-y-8 text-left">
-              <p class="text-lg font-medium">
+            <div className="ml-8 flex flex-col justify-between space-y-8 text-left">
+              <p className="text-lg font-medium">
                 SDH has known for its chicken education system and indeed they
                 fulfill it. SDH has changed and shaped many aspects in my life.
                 Academically, SDH gave me the push to work hard labours and
                 provide for my job as a professional snake milker.
               </p>
-              <div class="font-medium">
-                <div class="text-yellow-500">Jovannes Adryan, Snake milker</div>
-                <div class="text-neutral-7000">
+              <div className="font-medium">
+                <div className="text-yellow-500">
+                  Jovannes Adryan, Snake milker
+                </div>
+                <div className="text-neutral-7000">
                   SDH Daan Mogot, Class of 1959
                 </div>
               </div>
