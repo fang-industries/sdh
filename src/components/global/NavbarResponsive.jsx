dd866427c0d3d79bb35c1fb7ebaa77630628dc95
Jason <pix3l.icious@gmail.com>
2022-05-01 20:47:50 +0700

    added nav items

--- src/components/global/NavbarResponsive.jsx
+++ src/components/global/NavbarResponsive.jsx
@@ -65,6 +65,136 @@ export default function (props) {
             ></motion.div>
           )}
         </AnimatePresence>
+        <AnimatePresence>
+          {active && (
+            <motion.div
+              className={`${
+                active ? "" : "hidden"
+              } fixed z-40 h-full w-2/3 bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-50 md:hidden`}
+              exit={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              initial={{ x: "-100%" }}
+              transition={{ type: "tween", duration: 0.2 }}
+            >
+              <div>
+                <Link to="/">
+                  <div
+                    className={`${
+                      props.active === "home" ? "font-bold text-yellow-500" : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon icon={faHome} className="my-auto" />
+                      <p className="font-medium">Home</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/about">
+                  <div
+                    className={`${
+                      props.active === "about"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon
+                        icon={faCircleInfo}
+                        className="my-auto"
+                      />
+                      <p className="font-medium">About</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/admissions">
+                  <div
+                    className={`${
+                      props.active === "admissions"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon
+                        icon={faGraduationCap}
+                        className="my-auto"
+                      />
+                      <p className="font-medium">Admissions</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/education">
+                  <div
+                    className={`${
+                      props.active === "education"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon
+                        icon={faChalkboard}
+                        className="my-auto"
+                      />
+                      <p className="font-medium">Education</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/community">
+                  <div
+                    className={`${
+                      props.active === "community"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon icon={faComments} className="my-auto" />
+                      <p className="font-medium">Community</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/newspaper">
+                  <div
+                    className={`${
+                      props.active === "newspaper"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon icon={faNewspaper} className="my-auto" />
+                      <p className="font-medium">Newspaper</p>
+                    </div>
+                  </div>
+                </Link>
+                <div className="mx-2 border-b border-neutral-300 dark:border-neutral-700"></div>
+                <Link to="/voices">
+                  <div
+                    className={`${
+                      props.active === "ourvoices"
+                        ? "font-bold text-yellow-500"
+                        : ""
+                    } py-3 px-4`}
+                  >
+                    <div className="flex flex-row justify-between">
+                      <FontAwesomeIcon
+                        icon={faMicrophone}
+                        className="my-auto"
+                      />
+                      <p className="font-medium">Our Voices</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
       <div className=" bg-neutral-100 bg-opacity-60 py-4 px-4 shadow-xl backdrop-blur-[8px] backdrop-filter dark:bg-neutral-800 dark:bg-opacity-60 md:hidden">
         <div className="flex flex-row justify-between">
@@ -82,264 +212,6 @@ export default function (props) {
           >
             <FontAwesomeIcon icon={faBars} className="text-xl" />
           </button>
-        </div>
-        <div className="hidden flex-row items-center justify-between pt-4 md:flex">
-          <div className="flex flex-row space-x-4">
-            <Link to="/">
-              <button
-                className={`${
-                  props.active === "home"
-                    ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                    : "text-neutral-900"
-                } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-              >
-                <FontAwesomeIcon
-                  icon={faHome}
-                  className="my-auto transform text-lg"
-                />
-                <p className="my-auto font-semibold">Home</p>
-              </button>
-            </Link>
-            <Link to="/about">
-              <Tippy
-                content={
-                  <div className="flex flex-col px-1 text-center">
-                    <Link to="/about#welcome">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faPeopleGroup}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">About Us</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/about#history">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faCalendarDay}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">History</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/about/networks">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faCircleNodes}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Networks</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/about/campuses">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faSchool}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Campuses</p>
-                      </div>
-                    </Link>
-                  </div>
-                }
-                placement="bottom"
-                interactive
-                animation="shift-away"
-              >
-                <button
-                  className={`${
-                    props.active === "about"
-                      ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                      : "text-neutral-900"
-                  } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-                >
-                  <FontAwesomeIcon
-                    icon={faCircleInfo}
-                    className="my-auto transform text-lg"
-                  />
-                  <p className="my-auto font-semibold">About</p>
-                </button>
-              </Tippy>
-            </Link>
-            <Link to="/admissions">
-              <Tippy
-                content={
-                  <div className="flex flex-col px-1 text-center">
-                    <Link to="/admissions#register">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faAddressCard}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Registration</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/admissions#visit">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faSchool}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Visit SDH</p>
-                      </div>
-                    </Link>
-                  </div>
-                }
-                placement="bottom"
-                interactive
-                animation="shift-away"
-              >
-                <button
-                  className={`${
-                    props.active === "admissions"
-                      ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                      : "text-neutral-900"
-                  } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-                >
-                  <FontAwesomeIcon
-                    icon={faGraduationCap}
-                    className="my-auto transform text-lg"
-                  />
-                  <p className="my-auto font-semibold">Admissions</p>
-                </button>
-              </Tippy>
-            </Link>
-            <Link to="/education">
-              <Tippy
-                content={
-                  <div className="flex flex-col px-1 text-center">
-                    <Link to="/education/academic">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faUserGraduate}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Academic Overview</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/education/kindergarten">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faBaby}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Kindergarten</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/education/kindergarten">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faAppleWhole}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Primary School</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/education/kindergarten">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faBookOpen}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Junior School</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/education/kindergarten">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faUserGraduate}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">Senior School</p>
-                      </div>
-                    </Link>
-                    <div className="border-[1px] border-b border-neutral-500 border-opacity-50" />
-                    <Link to="/education/kindergarten">
-                      <div className="flex flex-row justify-between p-2 font-medium transition duration-300 ease-in-out hover:text-yellow-400">
-                        <FontAwesomeIcon
-                          icon={faShapes}
-                          className="my-auto mr-4"
-                        />
-                        <p className="my-auto ml-4">After School Activities</p>
-                      </div>
-                    </Link>
-                  </div>
-                }
-                placement="bottom"
-                interactive
-                animation="shift-away"
-              >
-                <button
-                  className={`${
-                    props.active === "education"
-                      ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                      : "text-neutral-900"
-                  } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-                >
-                  <FontAwesomeIcon
-                    icon={faChalkboard}
-                    className="my-auto transform text-lg"
-                  />
-                  <p className="my-auto font-semibold">Teaching & Learning</p>
-                </button>
-              </Tippy>
-            </Link>
-          </div>
-          <div className="flex flex-row space-x-4">
-            <Link to="/community">
-              <button
-                className={`${
-                  props.active === "community"
-                    ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                    : "text-neutral-900"
-                } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-              >
-                <FontAwesomeIcon
-                  icon={faComments}
-                  className="my-auto transform text-lg"
-                />
-                <p className="my-auto font-semibold">Community</p>
-              </button>
-            </Link>
-            <Link to="/news">
-              <button
-                className={`${
-                  props.active === "newspaper"
-                    ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                    : "text-neutral-900"
-                } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-              >
-                <FontAwesomeIcon
-                  icon={faNewspaper}
-                  className="my-auto transform text-lg"
-                />
-                <p className="my-auto font-semibold">Newspaper</p>
-              </button>
-            </Link>
-            <Link to="/voices">
-              <button
-                className={`${
-                  props.active === "ourvoices"
-                    ? "bg-neutral-900 bg-opacity-[0.33] text-neutral-50 dark:bg-neutral-100 dark:bg-opacity-[0.33] dark:text-yellow-400"
-                    : "text-neutral-900"
-                } transtion flex flex-row space-x-2 rounded px-3 py-2 duration-300 hover:bg-neutral-900 hover:bg-opacity-20 dark:text-neutral-50 dark:hover:bg-neutral-100 dark:hover:bg-opacity-20`}
-              >
-                <FontAwesomeIcon
-                  icon={faMicrophone}
-                  className="my-auto transform text-lg"
-                />
-                <p className="my-auto font-semibold">Our Voices</p>
-              </button>
-            </Link>
-          </div>
         </div>
       </div>
     </nav>
