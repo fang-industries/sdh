36d7862596e7bf05f327ff95ccf8ec03b4b888f2
Jason <pix3l.icious@gmail.com>
2022-05-02 11:37:56 +0700

    add licenses page

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from './pages/Index'
+import Licenses from "./pages/Licenses";
 
-import './assets/styles/tailwind.css'
-import './assets/styles/fonts.css'
+import "./assets/styles/tailwind.css";
+import "./assets/styles/fonts.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/licenses" element={<Licenses />} />
         <Route path="*" element={<Index />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
---
+++ src/pages/Licenses.jsx
@@ -0,0 +1,28 @@
+import Container from "../components/global/Container";
+import Image from "../components/pages/licenses/Image";
+import LibraryCard from "../components/modules/pages/licenses/libraries/Card";
+import Join from "../components/pages/home/Join";
+
+export default function () {
+  return (
+    <Container title="License">
+      <Image />
+      <div className="mx-4 my-8 space-y-8 md:my-16 md:mx-16">
+        <div>
+          <div className="flex flex-row">
+            <p className="text-3xl font-semibold text-sky-500">Open-source</p>
+          </div>
+          <p className="text-4xl font-bold">License Notices</p>
+        </div>
+        <div className="space-y-4">
+          <p className="">
+            With all joking aside, this project was made possible through the
+            help of open-source projects, and we would like to thank them
+            through attribution on this page.
+          </p>
+        </div>
+      </div>
+      <Join />
+    </Container>
+  );
+}
