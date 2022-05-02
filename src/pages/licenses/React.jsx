3fd6e24f0e4e47146fdf9ee230dedcd0161343fd
Jason <pix3l.icious@gmail.com>
2022-05-02 12:19:33 +0700

    first license goes 2 react

---
+++ src/pages/licenses/React.jsx
@@ -0,0 +1,11 @@
+import Container from "../../components/modules/pages/licenses/container/License";
+
+export default function () {
+  return (
+    <Container
+      name="React"
+      copyright="Facebook, Inc. and its affiliates"
+      license="MIT"
+    ></Container>
+  );
+}
