69df09867748bb868dc4d504b4bcbda7571d453b
jason <pix3l.icious@gmail.com>
2022-05-08 11:55:04 +0700

    i frogor skull

--- src/components/pages/licenses/Image.jsx
+++ src/components/pages/licenses/Image.jsx
@@ -2,7 +2,7 @@ import Background from "../../../assets/media/images/pages/licenses/main-bg.jpg"
 export default function () {
   return (
     <img
-      src="https://raw.githubusercontent.com/sekolah-daging-habituasi/assets/main/pages/licenses/main-bg.jpg"
+      src={Background}
       alt="Page background"
       className="aspect-[21/9] object-cover"
     />
