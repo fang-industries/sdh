ba37fb343c556da59d72f5b8e0a48dc7e6b76d44
jason <pix3l.icious@gmail.com>
2022-05-17 12:11:26 +0700

    update text

--- src/pages/err/NotFound.jsx
+++ src/pages/err/NotFound.jsx
@@ -4,7 +4,10 @@ import Join from "../../components/global/Join";
 export default function () {
   return (
     <Container title="404 Not Found">
-      <p className=" px-4 py-8 text-center text-3xl font-bold md:px-16">
+      <p className="px-4 pt-8 text-3xl font-semibold md:px-16">
+        Oh hey, what are you doing here?
+      </p>
+      <p className="px-4 pb-8 pt-4 text-xl md:px-16">
         It looks like you found a page that doesn't exist yet. Make sure to
         check back later to see if it has been created!
       </p>
