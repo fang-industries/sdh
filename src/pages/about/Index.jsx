e4b2c18b46bc938b9a196eccc2e4e096b006b7d4
jason <pix3l.icious@gmail.com>
2022-05-19 14:30:35 +0700

    index

--- src/pages/about/Index.jsx
+++ src/pages/about/Index.jsx
@@ -1,10 +1,11 @@
 import Container from "../../components/global/Container";
-import Image from "../../components/pages/about/index/Image";
-import About from "../../components/pages/about/index/About";
-import History from "../../components/pages/about/index/History";
-import Verse from "../../components/pages/about/index/Verse";
-import Improvement from "../../components/pages/about/index/Improvement";
-import Development from "../../components/pages/about/index/Development";
+import Background from "../../assets/media/images/pages/about/index/main-bg.webp";
+import About from "../../components/pages/about/About";
+import History from "../../components/pages/about/History";
+import Statistics from "../../components/pages/about/Statistics";
+import Alumni from "../../components/pages/about/Alumni";
+import AlumniResponsive from "../../components/pages/about/responsive/Alumni";
+import News from "../../components/pages/about/News";
 import Join from "../../components/global/Join";
 
 export default function () {
@@ -20,6 +21,13 @@ export default function () {
       <Verse />
       <Improvement />
       <Development />
+      <img src={Background} className="aspect-[24/9] w-full object-cover" />
+      <About />
+      <History />
+      <Statistics />
+      <Alumni />
+      <AlumniResponsive />
+      <News />
       <Join />
     </Container>
   );
