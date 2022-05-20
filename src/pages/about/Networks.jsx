967f4b160e1667ad113b3f432dc1720d2497b1e3
jason <pix3l.icious@gmail.com>
2022-05-20 10:54:22 +0700

    udpate imports

--- src/pages/about/Networks.jsx
+++ src/pages/about/Networks.jsx
@@ -1,22 +1,18 @@
 import Container from "../../components/global/Container";
 import Background from "../../assets/media/images/pages/about/networks/handshake.webp";
-import About from "../../components/pages/about/About";
-import History from "../../components/pages/about/History";
-import Quote from "../../components/pages/about/Quote";
-import Improvement from "../../components/pages/about/Improvement";
-import Development from "../../components/pages/about/Development";
+import About from "../../components/pages/about/index/About/About";
+import History from "../../components/pages/about/index/History";
+import Quote from "../../components/pages/about/index/Quote";
+import Improvement from "../../components/pages/about/index/Improvement";
+import Development from "../../components/pages/about/index/Development";
 import Join from "../../components/global/Join";
 
 export default function () {
   return (
     <Container title="About">
       <img src={Background} className="aspect-[24/9] w-full object-cover" />
-      <div id="welcome">
-        <About />
-      </div>
-      <div id="history">
-        <History />
-      </div>
+      <About />
+      <History />
       <Quote />
       <Improvement />
       <Development />
